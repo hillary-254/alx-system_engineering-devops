f2f2dc5cc8b3af8401cdddd02b428f3ffe403de3
hillary-254 <hillarykim001@gmail.com>
2023-09-22 22:32:55 +0300

    fixing the code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Kills a process named 'killmenow' using Puppet
 
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  refreshonly => true,
-  subscribe   => File['/path/to/killmenow_script'], # Replace with the actual path to your script
+exec { 'pkill':
+  command     => 'pkill killmenow',
+  path     => '/usr/bin/',
 }
