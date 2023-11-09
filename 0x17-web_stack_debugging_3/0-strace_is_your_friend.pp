b0f138848c319d521564320ed29802ef9c29d580
hillary-254 <hillarykim001@gmail.com>
2023-11-09 18:20:43 +0300

    0. Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes server error(500) when GET HTTP method is requested to Apache web server
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
